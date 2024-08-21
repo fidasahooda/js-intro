f75ed8653746abb56540fa834867894a587588f8
fidasahooda <fidasahoodap@gmail.com>
2024-08-21 21:55:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.write("Hello world");
+document.write("<br>");
+document.write("Hello world");
+console.log("This is a letter");
+console.warn("warning");
+console.error("error");
+//single line comment
+/*multiline
+comment*/
+
+//let var const
+let person;
+person="fida";
+console.log(person);
+
+//hoisting
+// console.log(b);
+// var b=10;
+// console.log(a);
+// let a=4;
